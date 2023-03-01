9f81dc5d8b9d9671353f564415a71c904c94aefd
Sairen Christian Buerano <sairen.buerano@carbonetes.com>
2023-03-01 09:51:56 +0800

    fix: add err checking on showLatestVersion

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -123,7 +123,10 @@ func Start(arguments *model.Arguments, cfg *config.Configuration) {
 	}
 
 	log.Printf("\nAnalysis finished in %.2fs", time.Since(start).Seconds())
-	update.ShowLatestVersion()
+	err := update.ShowLatestVersion()
+	if err != nil {
+		log.Printf("Error on show latest version: %v", err)
+	}
 	credits.Show()
 }
 
