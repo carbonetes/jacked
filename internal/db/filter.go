a4c01e84f45c6b92ce26f7c118e2c84c1aad5aa4
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-03-21 23:13:42 +0800

    refactor: remove unnecessary codes on filter

--- internal/db/filter.go
+++ internal/db/filter.go
@@ -9,11 +9,6 @@ import (
 	"golang.org/x/exp/slices"
 )
 
-const (
-	Cvssv3BaseMethod string = "3.1"
-	Cvssv2BaseMethod string = "2"
-)
-
 var (
 	indexes               []int
 	enabledfilterSeverity bool
@@ -77,31 +72,14 @@ func filterCVE(id string, cves []string, index int) {
 }
 
 // Filter all severities listed in vulnerability ignore list
-func filterSeverity(cvss []model.Cvss, ignore *config.Vulnerability, vulnerability model.Vulnerability, index int) {
+func filterSeverity(cvss model.CVSS, ignore *config.Vulnerability, vulnerability model.Vulnerability, index int) {
 
-	if cvss != nil {
-		for _, cvss := range cvss {
-			for _, severity := range ignore.Severity {
-				if strings.EqualFold(cvss.Method, Cvssv3BaseMethod) && cvss.Score > 0 && strings.EqualFold(cvss.Severity, severity) {
-					if !slices.Contains(indexes, index) {
-						indexes = append(indexes, index)
-					}
-				} else if strings.EqualFold(cvss.Method, Cvssv2BaseMethod) && cvss.Score > 0 && strings.EqualFold(cvss.Severity, severity) {
-					if !slices.Contains(indexes, index) {
-						indexes = append(indexes, index)
-					}
-				} else {
-					if strings.EqualFold(severity, "Unknown") && cvss.Score > 0 {
-						if !slices.Contains(indexes, index) {
-							indexes = append(indexes, index)
-						}
-					}
-				}
+	for _, severity := range ignore.Severity {
+		if strings.EqualFold(cvss.Severity, severity) {
+			if !slices.Contains(indexes, index) {
+				indexes = append(indexes, index)
 			}
 		}
-	} else {
-		if !slices.Contains(indexes, index) {
-			indexes = append(indexes, index)
-		}
 	}
+
 }
