301ca72190c7ac35fca7fe0142c9bb655a1f05d9
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2023-06-13 09:43:36 +0800

    feat: add public function for db file checking

---
+++ pkg/core/db/db.go
@@ -0,0 +1,11 @@
+package db
+
+import "github.com/carbonetes/jacked/internal/db"
+
+func Check(forceUpdate bool) {
+	if forceUpdate {
+		db.DBCheck(false, true)
+		return
+	}
+	db.DBCheck(false, false)
+}
