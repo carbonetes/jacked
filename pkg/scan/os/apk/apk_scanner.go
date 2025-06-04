2330dbd604743f2ef8830fb9fdb81458fa37e605
Ian Jhumel Bautista <ian.bautista@carbonetes.com>
2025-06-04 09:42:54 +0800

    feat: added apk vulnerability scanner

---
+++ pkg/scan/os/apk/apk_scanner.go
@@ -0,0 +1,67 @@
+package apk
+
+import (
+	"github.com/CycloneDX/cyclonedx-go"
+	"github.com/carbonetes/jacked/internal/db"
+	"github.com/carbonetes/jacked/internal/helper"
+	"github.com/carbonetes/jacked/internal/log"
+	v3 "github.com/carbonetes/jacked/pkg/model/cdx"
+	"github.com/carbonetes/jacked/pkg/version"
+)
+
+// apk Scanner implements scan.Scanner for dpkg packages.
+type Scanner struct {
+	store db.Store
+}
+
+func NewScanner(store db.Store) *Scanner {
+	return &Scanner{
+		store: store,
+	}
+}
+
+func (s *Scanner) Scan(bom *cyclonedx.BOM) ([]cyclonedx.Vulnerability, error) {
+	var results []cyclonedx.Vulnerability
+	if bom == nil || bom.Components == nil || len(*bom.Components) == 0 {
+		return results, nil
+	}
+
+	for _, c := range *bom.Components {
+		if c.Properties == nil {
+			continue
+		}
+		if helper.GetComponentType(c.Properties) != "apk" {
+			continue
+		}
+		vulns := s.store.ApkSecDBMatch(c.Name)
+		if vulns == nil {
+			continue
+		}
+
+		apkVersion, err := version.NewApkVersion(c.Version)
+		if err != nil {
+			continue
+		}
+
+		for _, vuln := range *vulns {
+			if vuln.Constraints == "" {
+				continue
+			}
+
+			match, err := apkVersion.Check(vuln.Constraints)
+			if err != nil {
+				log.Errorf("error checking apk version %s against constraint %s: %v", c.Version, vuln.Constraints, err)
+				continue
+			}
+
+			if match {
+				vex := v3.ToVex(&vuln, &c, vuln.Constraints)
+				if vex != nil {
+					results = append(results, *vex)
+				}
+			}
+		}
+	}
+
+	return results, nil
+}
